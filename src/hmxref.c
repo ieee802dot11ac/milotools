3610cca4df2ccce049f07bd10738749e5ac2c39b
ieee 802.11ac <sillybegani@gmail.com>
2023-04-22 00:24:13 -0700

    it doesn't segfault. progress

--- src/hmxref.c
+++ src/hmxref.c
@@ -1,17 +1,19 @@
 #include "hmxref.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 HX_EXTERNAL_REFERENCE extref_from_file(FILE *file)
 {
 	uint32_t len;
 	HX_EXTERNAL_REFERENCE ret;
+	ret.refName = malloc(69);
 	fread(&len, 4, 1, file);
 	ret.strLen = len;
-	if (len == 0) {
-		ret.refName = (char*)"\0";
-	} else {
+	if (len != 0) {
 		for (uint32_t i = 0; i < len; i++)
 			ret.refName[i] = fgetc(file);
+	} else {
+		ret.refName = (char*)"\0";
 	}
 	return ret;
 }
