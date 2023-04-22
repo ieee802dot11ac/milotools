75fd4056a3c621a28996f910a263887fdc26b42e
Zephi Sero <sUPvJzqAXmpFjzhgK0qF14jdxdjdKOnFybT5IlmF@pm.me>
2023-04-21 23:55:50 -0500

    Renamed some things

--- meshconverter.c
+++ meshconverter.c
@@ -4,9 +4,9 @@
 #include <stdint.h>
 #include "hmxstuff.h"
 
-FILE* workingmesh;
+FILE* hxMeshFile;
 FILE* objtowriteto;
-HX_MESH_FILE_GH meshFile;
+HX_MESH_FILE_GH hxMeshData;
 
 uint64_t fsize(FILE* file) {
     uint64_t curr = ftell(file);
@@ -21,31 +21,31 @@ int main(int argc, char** argv) {
         printf("no file, so sad\nusage: %s <mesh_file>\n", argv[0]); 
         return -1;
     }
-    workingmesh = fopen(argv[1], "r");
-    if (workingmesh == NULL) {
+    hxMeshFile = fopen(argv[1], "r");
+    if (hxMeshFile == NULL) {
         printf("couldn't open file, so sad\n"); 
         fcloseall();
         return -1;
     }
-    fread(&meshFile, 1, fsize(workingmesh), workingmesh);
-    if (meshFile.version != 25) {
+    fread(&hxMeshData, 1, fsize(hxMeshFile), hxMeshFile);
+    if (hxMeshData.version != 25) {
         printf("bad file, so sad\n"); 
         fcloseall();
         return -1;
     }
-    printf("mesh ver: (GH1 is 25) %i", meshFile.version);
-    fread(&meshFile.location.localTransMtx, sizeof(float), 24, workingmesh);
+    printf("mesh ver: (GH1 is 25) %i", hxMeshData.version);
+    fread(&hxMeshData.location.localTransMtx, sizeof(float), 24, hxMeshFile);
     for (int i = 0; i < 12; i++) {
         if (i % 4 == 0) printf("\n");
-        printf("%f ", meshFile.location.localTransMtx[i]);
+        printf("%f ", hxMeshData.location.localTransMtx[i]);
     }
     for (int i = 0; i < 12; i++) {
         if (i % 4 == 0) printf("\n");
-        printf("%f ", meshFile.location.worldTransMtx[i]);
+        printf("%f ", hxMeshData.location.worldTransMtx[i]);
     }
     printf("\n");
-    for (int i = 0; i < meshFile.vertCount; i++) {
-        printf("vert index %i: x %f y %f z %f", i, meshFile.vertTable[i].X, meshFile.vertTable[i].Y, meshFile.vertTable[i].Z);
+    for (int i = 0; i < hxMeshData.vertCount; i++) {
+        printf("vert index %i: x %f y %f z %f", i, hxMeshData.vertTable[i].X, hxMeshData.vertTable[i].Y, hxMeshData.vertTable[i].Z);
     }
     fcloseall();
     return 0;
