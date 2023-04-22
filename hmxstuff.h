6a2c1798c9902bb1aaf76509413061addf8e22a0
Zephi Sero <sUPvJzqAXmpFjzhgK0qF14jdxdjdKOnFybT5IlmF@pm.me>
2023-04-22 00:46:50 -0500

    Clean up hmxstuff.h a little

--- hmxstuff.h
+++ hmxstuff.h
@@ -4,24 +4,19 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-struct HX_EXTERNAL_REFERENCE {
+typedef struct {
     uint32_t strLen;
     char* refName;
-};
+} HX_EXTERNAL_REFERENCE;
 
-typedef struct HX_EXTERNAL_REFERENCE HX_EXTERNAL_REFERENCE;
-
-struct HX_SPHERE {
+typedef struct HX_SPHERE {
 	float x;
 	float y;
 	float z;
 	float r;
-};
+} HX_SPHERE;
 
-typedef struct HX_SPHERE HX_SPHERE;
-
-typedef enum
-{
+typedef enum {
     kConstraintNone,
     kConstraintLocalRotate,
     kConstraintParentWorld,
@@ -33,41 +28,37 @@ typedef enum
     kConstraintFastBillboardXYZ
 } CONSTRAINT_ENUM; // i'm just stealing shit at this point, sorry cisco
 
-typedef enum
-{
+typedef enum {
     kVolumeEmpty,
     kVolumeTriangles,
     kVolumeBSP,
     kVolumeBox
 } VOLUME_ENUM;
 
-typedef enum
-{
+typedef enum {
     kMutableNone = 0,
     kMutableVerts = 31,
     kMutableFaces = 32,
     kMutableAll = 63
 } MUTABLE_ENUM;
 
-struct HX_TRANS {
+typedef struct {
 	uint32_t version; // should be 8
 	float localTransMtx[12]; // local transformation matrix
 	float worldTransMtx[12]; // world transformation matrix
 	
 	// i don't even know but trans so based
-	uint32_t trans_count;
-	HX_EXTERNAL_REFERENCE* trans_objects; // ????? completely optional but if it's there i don't want this thing choking and dying
+	uint32_t transCount;
+	HX_EXTERNAL_REFERENCE* transObjects; // ????? completely optional but if it's there i don't want this thing choking and dying
 
 	// parent mesh stuff
 	CONSTRAINT_ENUM constraint;
 	HX_EXTERNAL_REFERENCE target;
-	bool preserve_scale;
+	bool preserveScale;
 	HX_EXTERNAL_REFERENCE parent;
-};
+} HX_TRANS;
 
-typedef struct HX_TRANS HX_TRANS;
-
-struct HX_VERT_GH {
+typedef struct {
     float X; // standard coord system
     float Y; // standard coord system
     float Z; // standard coord system
@@ -80,19 +71,15 @@ struct HX_VERT_GH {
     float A; // 0-1, usually like half cause hmx
     float U; // texture space stuff
     float V; // texture space stuff
-};
+} HX_VERT_GH;
 
-typedef struct HX_VERT_GH HX_VERT_GH;
-
-struct HX_TRI_GH {
+typedef struct {
     uint16_t index0;
     uint16_t index1;
     uint16_t index2;
-};
+} HX_TRI_GH;
 
-typedef struct HX_TRI_GH HX_TRI_GH;
-
-struct HX_BMP_HEADER_GH {
+typedef struct {
     uint8_t magic;
     uint8_t bpp;
     uint32_t imgfmt;
@@ -101,23 +88,23 @@ struct HX_BMP_HEADER_GH {
     uint16_t height;
     uint16_t bytesperline;
     uint8_t padding2[19];
-};
+} HX_BMP_HEADER_GH;
 
-struct HX_MESH_FILE_GH {
+typedef struct {
 	// starting stuff
 	uint32_t version; // should be 25
 	HX_TRANS location;
 
 	// ????? idk bullet makes me crave death
-	uint32_t alwaysone;
-	uint8_t prollyabool;
-	uint32_t ignorefornow;
+	uint32_t alwaysOne;
+	uint8_t prollyABool;
+	uint32_t ignoreForNow;
 	HX_SPHERE bounding;
 	HX_EXTERNAL_REFERENCE matName;
 
 	// "geometry owner" (???????)
-	HX_EXTERNAL_REFERENCE geometryowner;
-	MUTABLE_ENUM mutable;
+	HX_EXTERNAL_REFERENCE geometryOwner;
+	MUTABLE_ENUM mutableParts;
 	VOLUME_ENUM volume;
 	uint8_t bsp; // no clue
 	
@@ -125,8 +112,6 @@ struct HX_MESH_FILE_GH {
 	HX_VERT_GH* vertTable;
 	uint32_t faceCount;
 	HX_TRI_GH* faceTable;
-};
-
-typedef struct HX_MESH_FILE_GH HX_MESH_FILE_GH;
+} HX_MESH_FILE_GH;
 
 #endif
