22a845b5cea9a7f23b7f673812a1d485a684d2b8
Zephi Sero <sUPvJzqAXmpFjzhgK0qF14jdxdjdKOnFybT5IlmF@pm.me>
2023-04-23 02:42:02 -0500

    Fixed some things I missed in the merge commit

--- lib/src/include/hmxcolor.h
+++ lib/src/include/hmxcolor.h
@@ -12,6 +12,10 @@ typedef struct {
 } HX_COLOR_4F;
 
 typedef struct {
+	float r,g,b;
+} HX_COLOR_3F;
+
+typedef struct {
 	u8 r,g,b,a;
 } HX_COLOR_8888;
 
@@ -32,6 +36,16 @@ INLINE HX_COLOR_8888 hmx_color_8888_from_color_4f(HX_COLOR_4F inColor)
 	};
 }
 
+INLINE HX_COLOR_8888 hmx_color_8888_from_color_3f(HX_COLOR_3F inColor)
+{
+	return (HX_COLOR_8888) {
+		.r = 255 * (inColor.r),
+		.g = 255 * (inColor.g),
+		.b = 255 * (inColor.b),
+		.a = 255,
+	};
+}
+
 INLINE HX_COLOR_4F hmx_color_4f_from_color_8888(HX_COLOR_8888 inColor)
 {
 	return (HX_COLOR_4F) {
@@ -39,6 +53,34 @@ INLINE HX_COLOR_4F hmx_color_4f_from_color_8888(HX_COLOR_8888 inColor)
 		.b = ((float) inColor.g) / 255.0,
 		.g = ((float) inColor.b) / 255.0,
 		.a = ((float) inColor.a) / 128.0, // weird half-alpha stuff with color8888
+	};
+}
+
+INLINE HX_COLOR_4F hmx_color_4f_from_color_3f(HX_COLOR_3F inColor)
+{
+	return (HX_COLOR_4F) {
+		.r = inColor.r,
+		.b = inColor.g,
+		.g = inColor.b,
+		.a = 1, // weird half-alpha stuff with color8888
+	};
+}
+
+INLINE HX_COLOR_3F hmx_color_3f_from_color_8888(HX_COLOR_8888 inColor)
+{
+	return (HX_COLOR_3F) {
+		.r = ((float) inColor.r) / 255.0,
+		.b = ((float) inColor.g) / 255.0,
+		.g = ((float) inColor.b) / 255.0
+	};
+}
+
+INLINE HX_COLOR_3F hmx_color_3f_from_color_4f(HX_COLOR_4F inColor)
+{
+	return (HX_COLOR_3F) {
+		.r = inColor.r,
+		.b = inColor.g,
+		.g = inColor.b,
 	};
 }
 
--- lib/src/include/hmxlight.h
+++ lib/src/include/hmxlight.h
@@ -6,17 +6,24 @@ extern "C" {
 #endif
 
 #include <stdint.h>
-#include "hmxenums.h"
 #include "hmxtransform.h"
 #include "hmxcolor.h"
 
+typedef enum {
+	kLightPoint,
+	kLightDirectional,
+	kLightFakeSpot,
+	kLightFloorSpot,
+	kLightShadowRef
+} LIGHT_TYPE_ENUM;
+
 typedef struct {
-    uint32_t version; // should be 3
-    HX_TRANSFORM trans;
-    HX_COLOR_3F color;
-    float intensity;
-    float range;
-    LIGHT_TYPE_ENUM type;
+	uint32_t version; // should be 3
+	HX_TRANSFORM trans;
+	HX_COLOR_3F color;
+	float intensity;
+	float range;
+	LIGHT_TYPE_ENUM type;
 } HX_LIGHT;
 
 #ifdef __cplusplus
